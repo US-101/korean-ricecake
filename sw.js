69c5889500325e81fa79c925b7c9f657b9fdab45
US-101 <theinsidesign@gmail.com>
2025-09-08 21:32:13 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -29,4 +29,9 @@ self.addEventListener("fetch", e => {
       }).catch(()=>caches.match("/index.html")))
     );
   }
+   if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker.register("sw.js");
+    });
+  }
 });
