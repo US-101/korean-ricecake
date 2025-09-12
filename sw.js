b47b92237a6298933e5d15ccf82347c9242fa2bd
US-101 <theinsidesign@gmail.com>
2025-09-12 23:53:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,47 @@
-self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
+const CACHE_NAME = 'korean-pronunciation-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/manifest.json',
+  '/icons/icon-512.png',
+  '/icons/p1.svg',
+  '/icons/p2.svg',
+  '/icons/p3.svg',
+  '/icons/p4.svg',
+  '/icons/micky.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(e.request);
+      })
+  );
+});
